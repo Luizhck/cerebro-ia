4f6a43d719a5397ecf34c390ed2754fc34bda913
Luizhck <luizlpff80@gmail.com>
2026-06-12 18:23:05 -0300

    Add loop control variables and return message

---
+++ plugins/loop_parar.lua
@@ -0,0 +1,8 @@
+_G.SpeedLoop = false
+_G.JumpLoop = false
+_G.GodLoop = false
+_G.FlyLoop = false
+_G.NoclipLoop = false
+_G.AutoFarmLoop = false
+_G.AutoKillLoop = false
+return "Todos os loops parados! ⏸️"
