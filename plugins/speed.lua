15277b3fae0cb8f1e784295339e9bc5ee6b8a65d
Luizhck <luizlpff80@gmail.com>
2026-06-12 18:22:05 -0300

    Update speed.lua

--- plugins/speed.lua
+++ plugins/speed.lua
@@ -1,1 +1,8 @@
-
+local args = ...
+local valor = tonumber(args) or 50
+local hum = game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:FindFirstChildOfClass("Humanoid")
+if hum then
+    hum.WalkSpeed = valor
+    return "Velocidade: " .. valor .. " ⚡"
+end
+return "Erro: sem personagem"
